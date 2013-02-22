09eb2f2b7a89106132a423484e93c9158809bbe0
synergy dev <synergye@codefi.re>
2013-02-22 12:02:19 -0800

    BoardConfig: proper ramdisk offset @SnowLeopardJB

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -17,7 +17,7 @@
 BOARD_KERNEL_CMDLINE := console=ttySHL0,115200,n8 androidboot.hardware=geeb lpj=67677
 BOARD_KERNEL_BASE := 0x80200000
 BOARD_KERNEL_PAGESIZE := 2048
-BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01600000
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000
 
 TARGET_KERNEL_CONFIG := j1a-perf_defconfig
 TARGET_KERNEL_SOURCE := kernel/lge/gee
