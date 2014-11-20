869eadd43ac46663efa647895ef4fdc09f98303d
Seth Shelnutt <shelnutt2@gmail.com>
2014-11-20 07:43:33 -0500

    cm-12.0 changes
    Change-Id: Ifd08d470346222b693ee1b99fc157d9897e463ea

--- device.mk
+++ device.mk
@@ -15,7 +15,7 @@
 #
 
 ## (2) Also get non-open-source specific aspects if available
-$(call inherit-product-if-exists, vendor/lge/e970/e970-vendor.mk)
+$(call inherit-product-if-exists, vendor/lge/e975/e975-vendor.mk)
 
 ## overlays
 DEVICE_PACKAGE_OVERLAYS += device/lge/e970/overlay
